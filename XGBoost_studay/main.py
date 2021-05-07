29463a18f2e326cc5357f2977e046642a120bba3
Gzk738 <798412226@qq.com>
2021-05-07 17:23:49 +0900

    实现单例预测

--- XGBoost_studay/main.py
+++ XGBoost_studay/main.py
@@ -7,7 +7,8 @@
 from xgboost import XGBClassifier
 import pandas as pd
 import numpy as np
-
+import xgboost
+from sklearn.metrics import accuracy_score
 def load_data():
     data = pd.read_csv('ensemble/Iris.csv')
     #前4/5作为训练集，后1/5作为测试集
@@ -28,11 +29,24 @@ def XGBoost():
     clf=XGBClassifier(base_score=0.5, booster='gbtree', learning_rate=0.05, max_depth=8, n_estimators=50)
     clf.fit(train_x, train_y)
     #测试
-    clf.
     result = clf.score(test_x, test_y)
     print("测试结果:", result)
 
+def load_model():
+    # 模型加载
+    xgb = xgboost.XGBClassifier()
+    xgb.load_model('xgb.model')
+    pre = xgb.predict(np.array([[91, 5.5, 2.6, 4.4, 1.2]]))
+    print(pre)
+    xgboost.to_graphviz(xgb, num_trees=0)
+    print(accuracy_score(np.array(pre).reshape(-1, 1), np.array(data['test_label']).reshape(-1, 1)))
+    '''
+    模型可视化
+    '''
+    plot_tree(xgb, num_trees=0)
+    plt.show()
 
 
 if __name__ == '__main__':
     XGBoost()
+    load_model()
