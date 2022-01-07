24cb65c4b20b0a9f22a2f41feeddb80014e01c3d
GZK <798412226@qq.com>
2022-01-07 19:02:52 +0900

    asdf

--- XAI/2022.01.05/test.py
+++
@@ -1,8 +0,0 @@
-import time
-temp = 0
-while(True):
-    temp +=1
-    time.sleep(0.5)
-    print(temp, "first")
-    if temp>9999:
-        temp = 0
--- XAI/2022.01.05/test1.py
+++
@@ -1,8 +0,0 @@
-import time
-temp = 0
-while(True):
-    temp +=1
-    time.sleep(0.5)
-    print(temp, "second")
-    if temp>9999:
-        temp = 0
